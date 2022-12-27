56aa23b51a52966f550e5a764c29c676946a49d9
AsutoshAcharya <asutosh246@gmail.com>
2022-12-27 16:26:59 +0530

    base design completed

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import Img from "../assets/img.png";
+import Attach from "../assets/attach.png";
 const Input = () => {
   return (
-    <div>
-      inp
+    <div className="input">
+      <input type="text" placeholder="Type message ...." />
+      <div className="send">
+        <img src={Attach} alt="" />
+        <input type="file" style={{ display: "none" }} id="file2" />
+        <label htmlFor="file2">
+          <img src={Img} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Message = () => {
   return (
-    <div>
-      message
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://avatars.githubusercontent.com/u/101463745?v=4"
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        {/* {<img
+          src="https://avatars.githubusercontent.com/u/101463745?v=4"
+          alt=""
+        />} */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
