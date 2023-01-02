d5c884d81cd7adeacdd77ef61ea632ae6f9f4cf7
AsutoshAcharya <asutosh246@gmail.com>
2023-01-02 12:13:56 +0530

    search functionality added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { auth } from "../firebase";
 import { signOut } from "firebase/auth";
+import { AuthContext } from "../context/AuthContext";
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
   return (
     <div className="navbar">
       <span className="logo">Green Chat</span>
       <div className="user">
-        <img
-          src="https://avatars.githubusercontent.com/u/101463745?v=4"
-          alt=""
-        />
-        <span>Asu</span>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
         <button
           onClick={() => {
             signOut(auth);
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,50 @@
 import React from "react";
-
+import { useState } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../firebase";
 const Search = () => {
+  const [username, setUserName] = useState("");
+
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const handleSearch = async () => {
+    try {
+      const q = query(collection(db, "users"), where("name", "==", username));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+        console.log(user);
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder="find user" />
-      </div>
-      <div className="userChat">
-        <img
-          src="https://avatars.githubusercontent.com/u/101463745?v=4"
-          alt=""
+        <input
+          type="text"
+          placeholder="find user"
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+          onKeyDown={handleKey}
         />
-        <div className="userChatInfo">
-          <span>Asutosh</span>
-        </div>
       </div>
+      {err && <span>User not found!</span>}
+      {user && (
+        <div className="userChat">
+          <img src={user.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{user.name}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
