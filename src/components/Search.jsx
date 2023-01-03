2387630d453358dded191c53ffa8e3dd678d36e4
AsutoshAcharya <asutosh246@gmail.com>
2023-01-03 13:15:07 +0530

    chats added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useState } from "react";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 const Search = () => {
   const [username, setUserName] = useState("");
 
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
+  const { currentUser } = useContext(AuthContext);
 
   const handleSearch = async () => {
     try {
@@ -24,6 +36,38 @@ const Search = () => {
   const handleKey = (e) => {
     e.code === "Enter" && handleSearch();
   };
+
+  const handleSelect = async () => {
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, "chats", combinedId));
+      if (!res.exists()) {
+        await setDoc(doc, (db, "chats", combinedId), { messages: [] });
+
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.name,
+            photoURL: user.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.name,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (err) {
+      setErr(true);
+    }
+  };
   return (
     <div className="search">
       <div className="searchForm">
@@ -38,7 +82,7 @@ const Search = () => {
       </div>
       {err && <span>User not found!</span>}
       {user && (
-        <div className="userChat">
+        <div className="userChat" onClick={handleSelect}>
           <img src={user.photoURL} alt="" />
           <div className="userChatInfo">
             <span>{user.name}</span>
