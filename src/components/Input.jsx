7127b9c99586c727c32446947033636a590f3724
AsutoshAcharya <asutosh246@gmail.com>
2023-01-09 19:19:52 +0530

    added realtime chat functionality

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,17 +1,75 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Img from "../assets/img.png";
 import Attach from "../assets/attach.png";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+import { Timestamp, arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { db, storage } from "../firebase";
+import { v4 as uuid } from "uuid";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+
 const Input = () => {
+  const [text, setText] = useState("");
+  const [img, setImg] = useState(null);
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const handleSend = async () => {
+    if (img) {
+      const storageRef = ref(storage, uuid());
+
+      const uploadTask = uploadBytesResumable(storageRef, img);
+
+      uploadTask.on(
+        (error) => {
+          //TODO:Handle Error
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateDoc(doc(db, "chats", data.chatId), {
+              messages: arrayUnion({
+                id: uuid(),
+                text,
+                senderId: currentUser.uid,
+                date: Timestamp.now(),
+                img: downloadURL,
+              }),
+            });
+          });
+        }
+      );
+    } else {
+      await updateDoc(doc(db, "chats", data.chatId), {
+        messages: arrayUnion({
+          id: uuid(),
+          text,
+          senderId: currentUser.uid,
+          date: Timestamp.now(),
+        }),
+      });
+    }
+  };
   return (
     <div className="input">
-      <input type="text" placeholder="Type message ...." />
+      <input
+        type="text"
+        placeholder="Type message ...."
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
       <div className="send">
         <img src={Attach} alt="" />
-        <input type="file" style={{ display: "none" }} id="file2" />
+        <input
+          type="file"
+          style={{ display: "none" }}
+          id="file2"
+          onChange={(e) => setImg(e.target.files[0])}
+        />
         <label htmlFor="file2">
           <img src={Img} alt="" />
         </label>
-        <button>Send</button>
+        <button onClick={handleSend}>Send</button>
       </div>
     </div>
   );
